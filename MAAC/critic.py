4d532c7abeb571b984eea072850e9d12ca45c770
shossain <shossain@vws77.vector.local>
2018-11-05 12:27:19 -0500

    Wrote critic with attention

--- MAAC/critic.py
+++ MAAC/critic.py
@@ -4,57 +4,97 @@ import torch
 import numpy as np
 import torch.nn as nn
 
-""" The Agent-dependent Critic. This is able to observe all states and actions and approximates the 
-Q function for each action that an agent can take.
+""" The Agent-dependent Critic.Each critic (one per agent) is a global critic and can access
+all observations from each agent (along with their actions). There is an attention layer for each
+critic that will pay special attention to whatever is deemed important
 """
 
-class AdvantageCritic(torch.nn.Module):
+class Global_Critic(torch.nn.Module):
     """
-    Efficient implementation of global Q-function for action dependent baseline,
-    input is joint state, fixed joint action excluding agent's action, one-hot agent index,
-    outputs the Q-value for every action that agent can take (in one forward pass), while keeping other actions fixed
+    Although we have multiple global critics, this can be efficiently computed with a single module
+    There are shared layers for all critics, and layers specific to each global critic. As such,
+    it will return a Q value for each agent.
     """
-    def __init__(self, hidden_size, observation_size, action_size, num_agents):
-        super(AdvantageCritic, self).__init__()
-        embedding_dim = 128
-        att_contribution_dim = 64
-
+    def __init__(self, observation_size, action_size, num_agents):
+        super(Global_Critic, self).__init__()
+        self.num_agents = num_agents
+        self.action_size = action_size
+        self.observation_size = observation_size
+        self.embedding_dim = 128
+        
         # unique to each agent's Q function
         self.g_functions = []
         self.f_functions = []
-        for i in range(num_agents):
+        for i in range(self.num_agents):
             self.g_functions.append(nn.Sequential(
-                torch.linear(observation_size+action_size, embedding_dim),
-                torch.nn.ReLU()
+                nn.Linear(observation_size+action_size, self.embedding_dim),
+                nn.ReLU()
             ))
             self.f_functions.append(nn.Sequential(
-                torch.linear(embedding_dim+attention_dim, att_contribution_dim),
-                torch.nn.ReLU()
-                torch.linear(64, 1),
+                nn.Linear(self.embedding_dim + 
+                    self.embedding_dim*self.num_agents, self.embedding_dim),
+                nn.ReLU(),
+                nn.Linear(self.embedding_dim, 1)
             ))
 
-        # Shared across all critics
-        Wq_tensor = torch.Parameter(torch.randn((n,embedding_dim, embedding_dim)))
-        Wk_tensor = torch.Parameter(torch.randn((n,embedding_dim, embedding_dim)))
-        
-
-    def forward(self, state_action):
+        # Shared across all critics 
+        # Matrix mult with a vector can be represented as a single linear layer
+        self.Wq_layers = [nn.Linear(self.embedding_dim, 
+            self.embedding_dim) for i in range(self.num_agents)]
+        self.Wk_layers = [nn.Linear(self.embedding_dim, 
+            self.embedding_dim) for i in range(self.num_agents)]
+        self.V_layers = [nn.Linear(self.embedding_dim, 
+            self.embedding_dim) for i in range(self.num_agents)]
+       
+    def forward(self, observation_vector, action_vector):
         """
         :param state_action: joint action, global state, one-hot agent index
         :return: Q-value for the joint state
         """
-        h = self.linear1(state_action)
-        return self.linear2(h)
+        batch_size = observation_vector[0].shape[0]
+        assert(len(observation_vector) == self.num_agents)
+        
+        # First compute the embedding
+        ei_s =[]
+        for i in range(len(observation_vector)):
+            combined = torch.cat([observation_vector[i], action_vector[i]], dim=1)
+            ei_s.append(self.g_functions[i](combined))
+        print(ei_s[0].shape)
+
+        xi_s = []
+        for i in range(len(observation_vector)):    # for each x_i
+            to_concat = []
+            for l in range(self.num_agents):             # for each of the multiple attention heads
+                query = self.Wq_layers[l](ei_s[i])
+                total = torch.zeros(batch_size, self.embedding_dim) 
+                for j in range(len(observation_vector)):
+                    if i != j:
+                        key = self.Wk_layers[l](ei_s[j])
+                        alpha_j = torch.zeros(batch_size, 1)
+                        for b in range(batch_size):
+                            alpha_j[b,:] = torch.nn.functional.softmax(key[b,:].dot(query[b,:]))
+                        assert(alpha_j.shape == (batch_size,1))
+                        v_j = torch.nn.functional.leaky_relu(self.V_layers[l](ei_s[j]))
+                        total += torch.mul(alpha_j,v_j)
+                assert(total.shape == (batch_size, self.embedding_dim))
+                to_concat.append(total)
+            xi = torch.cat(to_concat, dim=1)
+            assert(xi.shape == (batch_size, self.embedding_dim*self.num_agents))
+            xi_s.append(xi)
+        
+        q_for_agent = []
+        for i in range(self.num_agents):
+            q_for_agent.append(self.f_functions[i](torch.cat([xi_s[i], ei_s[i]], dim=1)))
+
+        return q_for_agent
 
 def unit_test():
-    test_critic = AdvantageCritic(10, 256, 3)
-    batch_size = 6
-    state_action = torch.randn((batch_size, 10))
-    output = test_critic.forward(state_action)
-    if (output.shape == (batch_size, 3)):
-        print("PASSED")
-
-    print(output.shape)
+    critic = Global_Critic(observation_size=10, action_size=5, num_agents=3)
+    obs_vector = [torch.randn((20, 10)) for i in range(3)]
+    action_vector = [torch.randn((20, 5)) for i in range(3)]
+    output = critic.forward(obs_vector, action_vector)
+    
+    print(output[0].shape)
 
 if __name__ == "__main__":
     unit_test()
