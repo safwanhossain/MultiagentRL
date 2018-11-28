Fixed off-by one error in saved joint action array, training seems to be nicer

COMA/actor.py
@@ -131,7 +131,7 @@ class GRUActor(torch.nn.Module):
         logits = self.linear(output)
 
         # compute eps-bounded softmax
-        softmax = torch.nn.functional.softmax(logits, dim=2)
+        softmax = torch.nn.functional.softmax(logits, dim=-1)
         return (1 - eps) * softmax + eps / self.action_size, h_state
 
     def init_weights(self, m):
@@ -169,7 +169,7 @@ class MLPActor(torch.nn.Module):
         logits = self.model.forward(input)
 
         # compute eps-bounded softmax
-        softmax = torch.nn.functional.softmax(logits, dim=2)
+        softmax = torch.nn.functional.softmax(logits, dim=-1)
         return (1 - eps) * softmax + eps / self.action_size
 
     def init_weights(self, m):

COMA/coma.py
@@ -20,7 +20,7 @@ things start working). In each episode (similar to "epoch" in normal ML parlance
 class COMA():
 
     def __init__(self, env, critic_arch, policy_arch, batch_size, seq_len, discount, lam, n_agents, action_size,
-                 obs_size, state_size, lr_critic=0.0005, lr_actor=0.0002):
+                 obs_size, state_size, lr_critic=0.0005, lr_actor=0.0002, alpha=0.1, flags=None):
         """
         :param batch_size:
         :param seq_len: horizon
@@ -44,6 +44,8 @@ class COMA():
         self.env = env
         self.lr_critic = lr_critic
         self.lr_actor = lr_actor
+        self.alpha = alpha
+        self.flags = flags
         self.metrics = {}
 
         self.params = {
@@ -54,6 +56,7 @@ class COMA():
             "lambda": lam,
             "lr_critic": lr_critic,
             "lr_actor": lr_actor,
+            "alpha": alpha,
                 }
 
         self.critic_arch = critic_arch
@@ -140,7 +143,7 @@ class COMA():
         diff_actions = torch.zeros_like(self.joint_action_state_pl)
 
         # initialize the advantage for each agent, by copying the Q values
-        advantage = [q_vals.clone().detach() for a in range(self.n_agents)]
+        advantage = [q_vals.clone().detach() for a in range(self.n_agents) ]
 
         self.actor_optimizer.zero_grad()
 
@@ -160,7 +163,7 @@ class COMA():
             # make a copy of the joint-action, state, to substitute different actions in it
             diff_actions.copy_(self.joint_action_state_pl)
 
-            # get the chosen action for each agent
+            # get the chosen action at the next time step for each agent
             action_index = a * self.action_size
             chosen_action = self.joint_action_state_pl[:, :, action_index:action_index+self.action_size]
 
@@ -176,6 +179,11 @@ class COMA():
                 Q_u = self.critic.forward(diff_actions)
 
                 advantage[a] -= Q_u*action_dist[:, :, u].unsqueeze_(-1)
+
+            # if using soft actor critic, add the policy's entropy to the advantage
+            if self.flags["SAC"]:
+                # add entropy term, for chosen action at next step, mind the timestep correspondence
+                advantage[a][:, 0:-1, :] += self.alpha * torch.sum(torch.log(action_dist[:, 1, :])*chosen_action[:, 1:, :], dim=-1)
 
             # loss is negative log of probability of chosen action, scaled by the advantage
             # the advantage is treated as a scalar, so the gradient is computed only for log prob
@@ -214,13 +222,7 @@ class COMA():
 
         # first compute the future discounted return at every step using the sequence of rewards
 
-        G = np.zeros((self.batch_size, self.seq_len, self.seq_len + 1))
-        # print('rewards', self.reward_seq_pl[0, :])
-
-        # apply discount and sum
-        total_return = self.reward_seq_pl.dot(np.fromfunction(lambda i: self.discount**i, shape=(self.seq_len,)))
-
-        # print(total_return[0])
+        G = np.zeros((self.batch_size, self.seq_len, self.seq_len))
 
         # initialize the first column with estimates from the target Q network
         predictions = self.target_critic.forward(self.joint_action_state_pl).squeeze()
@@ -232,7 +234,7 @@ class COMA():
         for t in range(self.seq_len - 1, -1, -1):
 
             # loop from one-step lookahead to pure MC estimate
-            for n in range(1, self.seq_len + 1):
+            for n in range(1, self.seq_len - 1):
 
                 # pure MC
                 if t + n > self.seq_len - 1:
@@ -248,17 +250,25 @@ class COMA():
 
         sum_loss = 0.
 
-        for t in range(self.seq_len-3, -1, -1):
+        # by moving backwards except for the last transition, compute targets and update critic
+        for t in range(self.seq_len-2, -1, -1):
 
             # vector of powers of lambda
-            weights = np.fromfunction(lambda i: lam ** i, shape=(self.seq_len - t,))
+            weights = np.fromfunction(lambda i: lam ** i, shape=(self.seq_len -1 - t,))
 
             # normalize
             weights = weights / np.sum(weights)
 
             print('t', t)
-            # n=1 1 step TD
-            targets[:, t] = torch.from_numpy(G[:, t, 1:self.seq_len-t+1].dot(weights))
+            targets[:, t] = torch.from_numpy(G[:, t, 1:self.seq_len - t].dot(weights))
+
+            # if using soft actor critic, compute the joint action dist for the next time step
+            if self.flags["SAC"]:
+                joint_action_dist = 1
+                for a in range(self.n_agents):
+                    joint_action_dist *= torch.log(self.agents[a].get_action_dist(self.actor_input_pl[a][:, t+1, :], eps=0))
+                targets[:, t] += self.alpha*joint_action_dist
+
             print('target', targets[0, t])
             pred = self.critic.forward(self.joint_action_state_pl[:, t]).squeeze()
             print('pred', pred[0])

COMA/coma_particle.py
@@ -20,12 +20,12 @@ def gather_rollouts(coma, eps):
     # Step 1: generate batch_size rollouts
     for i in range(coma.batch_size):
 
-        # initialize action to noop
+        #initialize action to noop
         joint_action = torch.zeros((coma.n_agents, coma.action_size))
         joint_action[:, 0] = 1
         coma.env.reset()
-
         coma.reset_agents()
+
 
         for t in range(coma.seq_len):
 
@@ -34,9 +34,6 @@ def gather_rollouts(coma, eps):
 
             # they all get the same reward, save the reward, divided by number of agents, to prevent large rewards and to compare with single agent experiments
             coma.reward_seq_pl[i, t] = reward_n[0] / coma.n_agents
-
-            # save the joint action for training
-            coma.joint_action_pl[i, t, :] = joint_action.flatten()
 
             # for each agent, save observation, compute next action
             for n in range(coma.n_agents):
@@ -49,7 +46,7 @@ def gather_rollouts(coma, eps):
                 # the global state consists of positions + velocity of agents, first 4 elements from obs
                 coma.global_state_pl[i, t, n * 4:4 * n + 4] = torch.from_numpy(obs_n[n][0:4])
 
-                # get distribution over actions, concatenate observation and prev action
+                # get distribution over actions, concatenate observation and prev action for actor training
                 obs_action = np.concatenate((obs_n[n][0:coma.obs_size], joint_action[n, :], agent_idx))
                 actor_input = torch.from_numpy(obs_action).view(1, 1, -1).type(torch.FloatTensor)
 
@@ -59,12 +56,16 @@ def gather_rollouts(coma, eps):
                 action=coma.agents[n].get_action(actor_input, eps=eps)
                 joint_action[n, :] = action
 
+                # save the next joint action for training
+                coma.joint_action_pl[i, t, :] = joint_action.flatten()
+
             # get the absolute landmark positions for the global state
             coma.global_state_pl[i, t, coma.n_agents * 4:] = torch.from_numpy(np.array(
                 [landmark.state.p_pos for landmark in coma.env.world.landmarks]).flatten())
 
 
     # concatenate the joint action, global state, set network inputs to torch tensors
+    # action taken at state s
     coma.joint_action_state_pl = torch.cat((coma.joint_action_pl, coma.global_state_pl), dim=-1)
 
     coma.joint_action_state_pl.requires_grad_(True)
@@ -106,18 +107,26 @@ def visualize(coma):
 
 if __name__ == "__main__":
 
-    n = 1
+    n = 2
     obs_size = 4 + 2*(n-1) + 2*n
     state_size = 4*n + 2*n
 
     env = marl_env.make_env('simple_spread', n_agents=n)
 
-    policy_arch = {'type': GRUActor, 'h_size': 128}
+    policy_arch = {'type': MLPActor, 'h_size': 128}
     critic_arch = {'h_size': 128, 'n_layers': 2}
 
+    # FLAGS:
+    SAC = False
+    TD_LAMDA = True
+    COMA_BASELINE = True
+    PARTIAL_OBS = True
+
+    flags = {"SAC":SAC, "TD_LAMBDA":TD_LAMDA, "COMA_BASELINE": COMA_BASELINE, "PARTIAL_OBS": PARTIAL_OBS}
+
     coma = COMA(env=env, critic_arch=critic_arch, policy_arch=policy_arch,
-                batch_size=60, seq_len=30, discount=0.8, lam=0.8, n_agents=n, action_size=5, obs_size=obs_size,
-                     state_size=state_size, lr_critic=0.0002, lr_actor=0.0001)
+                batch_size=60, seq_len=100, discount=0.8, lam=0.8, n_agents=n, action_size=5, obs_size=obs_size,
+                     state_size=state_size, lr_critic=0.0002, lr_actor=0.0001, flags=flags)
 
     experiment = Experiment(api_key='1jl4lQOnJsVdZR6oekS6WO5FI', project_name="COMA", \
                                 auto_param_logging=False, auto_metric_logging=False,
@@ -129,6 +138,11 @@ if __name__ == "__main__":
     experiment.log_multiple_params(coma.policy_arch)
     experiment.log_multiple_params(coma.critic_arch)
 
+    if not SAC:
+        eps = 0.15
+
+    else:
+        eps = 0
     # visualize(coma)
     try:
         for e in range(4000):
@@ -136,7 +150,7 @@ if __name__ == "__main__":
                 print('e', e)
                 coma.update_target()
 
-            gather_rollouts(coma, eps=0.05 - e*0.05/4000)
+            gather_rollouts(coma, eps=eps - e*eps/4000)
 
             critic_loss = coma.fit_critic()
             print("loss", critic_loss)